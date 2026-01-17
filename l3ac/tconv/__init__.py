36507478ba62dffee2c7377e008980f4243f07ec
Hyeongyong Choi <chy1218@snu.ac.kr>
2026-01-17 20:54:13 +0900

    Add causal parameter to V3FirstBlock and EnhanceBlock

--- l3ac/tconv/__init__.py
+++ l3ac/tconv/__init__.py
@@ -6,9 +6,16 @@ from . import base
 
 
 class V3FirstBlock(base.BaseBlock):  # (1, 5, 11, 21, 45)
-    def __init__(self, target_dim, conv_kernels=(7, 7, 7, 7, 7), pool_kernels=(1, 5, 11, 21, 45), dilation_rate=7):
+    def __init__(
+        self,
+        target_dim,
+        conv_kernels=(7, 7, 7, 7, 7),
+        pool_kernels=(1, 5, 11, 21, 45),    
+        dilation_rate=7,
+        causal: bool = False,
+    ):
         h_dim = len(pool_kernels) * 4
-        super().__init__(h_dim, conv_kernels, pool_kernels, dilation_rate=dilation_rate)
+        super().__init__(h_dim, conv_kernels, pool_kernels, dilation_rate=dilation_rate, causal=causal)
         self.conv_1 = Conv1d(h_dim, h_dim * 4, kernel_size=1)
         self.act = nn.GELU()
         self.conv_2 = Conv1d(h_dim * 4 + 1, target_dim, kernel_size=1)
@@ -22,17 +29,20 @@ class V3FirstBlock(base.BaseBlock):  # (1, 5, 11, 21, 45)
         return y
 
 
-FirstBlock = lambda dim: (
-    V3FirstBlock(dim, conv_kernels=(7, 7, 7, 7, 7), pool_kernels=(1, 5, 11, 21, 45), dilation_rate=99)
-)
-
+def FirstBlock(dim, causal: bool = False):
+    return V3FirstBlock(
+        dim,
+        conv_kernels=(7, 7, 7, 7, 7),
+        pool_kernels=(1, 5, 11, 21, 45),
+        dilation_rate=99,
+        causal=causal,
+    )
 
 class EnhanceBlock(base.BaseBlock):
-    def __init__(self, dim):
-        super().__init__(4, conv_kernels=(7, 7, 7, 7), pool_kernels=(1, 3, 5, 9))
+    def __init__(self, dim, causal: bool = False):
+        super().__init__(4, conv_kernels=(7, 7, 7, 7), pool_kernels=(1, 3, 5, 9), causal=causal)
         self.dim = dim
         self.merge_layer = nn.Sequential(
-            # nn.LeakyReLU(),  # ! if active or use InstanceNorm1d
             nn.InstanceNorm1d(4, affine=True),
             nn.Conv1d(4, self.dim, kernel_size=1)
         )
